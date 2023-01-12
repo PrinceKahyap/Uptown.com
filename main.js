3c4e6e710df92808fd468ac59a6ddfd3d6789e5a
Princekashyap <vasukashyap08@gmail.com>
2023-01-12 15:37:48 +0530

    i add some section in home page

---
+++ main.js
@@ -0,0 +1,51 @@
+const counters = document.querySelectorAll('.counter')
+       counters.forEach(counter => {
+           counter.innerText = '0'
+           const updateCounter = () => {
+               const target = +counter.getAttribute('data-target')
+               const c = +counter.innerText
+               const increment = target / 200
+               if (c < target){
+                   counter.innerText = `${Math.ceil(c + increment)}`
+                   setTimeout(updateCounter,5)
+               }else{
+                   counter.innerText = target
+               }
+           }
+           updateCounter()
+       });
+window.addEventListener("scroll",function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky",window.scrollY > 200)
+})
+// owlCarousel 
+$(".carousel").owlCarousel({
+    margin: 10,
+    loop: true,
+    autoplay:  false,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive:{
+     0:{
+         items: 1,
+         nav: false
+     },
+     600:{
+         items: 2,
+         nav: false
+     },
+     1000:{
+         items: 3,
+         nav: false
+     },
+    }
+ });
+
+// Current year for footer 
+const year = document.querySelector("#current-year");
+year.innerHTML = new Date().getFullYear();
+
+let preloader = document.getElementById("loading");
+function myFunction(){
+    preloader.style.display = "none";
+};    
