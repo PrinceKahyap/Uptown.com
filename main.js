8138c478386f7e2ab793275464284d0a8839bd8b
Princekashyap <vasukashyap08@gmail.com>
2023-01-18 14:13:26 +0530

    i make website responsive

--- main.js
+++ main.js
@@ -1,51 +1,70 @@
 const counters = document.querySelectorAll('.counter')
-       counters.forEach(counter => {
-           counter.innerText = '0'
-           const updateCounter = () => {
-               const target = +counter.getAttribute('data-target')
-               const c = +counter.innerText
-               const increment = target / 200
-               if (c < target){
-                   counter.innerText = `${Math.ceil(c + increment)}`
-                   setTimeout(updateCounter,5)
-               }else{
-                   counter.innerText = target
-               }
-           }
-           updateCounter()
-       });
-window.addEventListener("scroll",function(){
+counters.forEach(counter => {
+    counter.innerText = '0'
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+        const increment = target / 200
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 5)
+        } else {
+            counter.innerText = target
+        }
+    }
+    updateCounter()
+});
+window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
-    header.classList.toggle("sticky",window.scrollY > 200)
+    header.classList.toggle("sticky", window.scrollY > 200)
 })
 // owlCarousel 
 $(".carousel").owlCarousel({
     margin: 10,
     loop: true,
-    autoplay:  false,
+    autoplay: false,
     autoplayTimeout: 2000,
     autoplayHoverPause: true,
-    responsive:{
-     0:{
-         items: 1,
-         nav: false
-     },
-     600:{
-         items: 2,
-         nav: false
-     },
-     1000:{
-         items: 3,
-         nav: false
-     },
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: false
+        },
     }
- });
+});
 
 // Current year for footer 
 const year = document.querySelector("#current-year");
 year.innerHTML = new Date().getFullYear();
 
 let preloader = document.getElementById("loading");
-function myFunction(){
+function myFunction() {
     preloader.style.display = "none";
-};    
+};
+
+// Hamburger 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+};
